1d3cff1401a6b95e660787f1334680a5d3153b6b
fabiensebban <fa.sebban@gmail.com>
2019-03-12 14:15:10 +0100

    First commit

---
+++ docker/rails/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Creating database if it's not present..."
+bin/rails db:create
+
+echo "Migrating database..."
+bin/rails db:migrate
+
+# If the container has been killed, there may be a stale pid file
+# preventing rails from booting up
+rm -f tmp/pids/server.pid
+
+exec "$@"
