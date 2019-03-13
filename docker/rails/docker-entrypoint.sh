135e6d309b108c8130b101478759d8cae0e1f391
fabiensebban <fa.sebban@gmail.com>
2019-03-13 09:44:07 +0100

    Remove database configuration

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Creating database if it's not present..."
-bin/rails db:create
+# echo "Creating database if it's not present..."
+# bin/rails db:create
 
-echo "Migrating database..."
-bin/rails db:migrate
+# echo "Migrating database..."
+# bin/rails db:migrate
 
 # If the container has been killed, there may be a stale pid file
 # preventing rails from booting up
