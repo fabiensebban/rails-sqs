f00e370ea4e2251aaca9b5b4d2dda7816d5586e4
fabiensebban <fa.sebban@gmail.com>
2019-03-24 12:28:23 +0100

    sqs reach home controller Now trying to reach Job

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def test
-    raise "I am in the home controller"
     FirstJob.perform_later "test string"
   end
 end
--- app/jobs/first_job.rb
+++ app/jobs/first_job.rb
@@ -2,7 +2,6 @@ class FirstJob < ApplicationJob
   queue_as :rails_sqs
 
   def perform(test)
-    logger.info "I am in the first job"
     raise "This is the first job : #{test}"
   end
 end
