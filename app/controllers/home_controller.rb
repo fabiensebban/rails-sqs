0188ab72007d4eed788662378c7de6fc859a16cb
fabiensebban <fa.sebban@gmail.com>
2019-03-24 21:14:16 +0100

    adding logger

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def test
+    logger.info 'i am in the Home controller'
     FirstJob.perform_later(test: 'test string')
     raise "Error after performing job"
   end
