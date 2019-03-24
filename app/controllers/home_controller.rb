2aa275df35ea3cb9996def725631f8a8a4456d04
fabiensebban <fa.sebban@gmail.com>
2019-03-24 22:06:24 +0100

    adding logs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def test
-    logger.info 'i am in the Home controller'
+    my_log = Logger.new("#{Rails.root}/log/my_controller.log")
+    my_log.info('i am in the Home controller')
     FirstJob.perform_later(test: 'test string')
     raise "Error after performing job"
   end
--- app/jobs/first_job.rb
+++ app/jobs/first_job.rb
@@ -2,7 +2,8 @@ class FirstJob < ApplicationJob
   queue_as :rails_sqs
 
   def perform(*test)
-    logger.info 'i am in the first job'
+    my_log = Logger.new("#{Rails.root}/log/my_job.log")
+    my_log.info('i am in the First Job')
     raise "This is the first job : #{test}"
   end
 end
