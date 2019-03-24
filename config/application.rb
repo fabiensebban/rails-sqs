14c9209c0041a316dfa70217d7a271e3eef1fe8e
fabiensebban <fa.sebban@gmail.com>
2019-03-24 20:11:19 +0100

    secret base key for active elastic job

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,15 @@ module RailsSqs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Gets env variables from the local_env.ym file
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+
     # Slack notifications
     config.middleware.use ExceptionNotification::Rack,
                           slack: {
@@ -38,5 +47,7 @@ module RailsSqs
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_elastic_job.secret_key_base = ENV['SECRET_KEY_BASE']
   end
 end
