4009d95fa097d4a1060a82e9a8af5e226828db99
fabiensebban <fa.sebban@gmail.com>
2019-03-24 12:13:16 +0100

    changing logger by raise in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def test
-    logger.info "I am in the home controller"
+    raise "I am in the home controller"
     FirstJob.perform_later "test string"
   end
 end
