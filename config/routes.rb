bc7f76a2ee1a4893ccdee32ba883384303f64c6a
fabiensebban <fa.sebban@gmail.com>
2019-04-03 12:13:35 +0200

    change route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/test', to: 'home#test'
+  # post '/test', to: 'home#test'
 
-  post '/', to: 'home#catchall'
+  # post '/', to: 'home#catchall'
 end
